6c83e473bce982c0b666a94c25f08ae9e40901d5
bhavyapatel <bhavyapatel4012@gmail.com>
2023-01-07 21:30:25 +0530

    changing EJS files and update forms and all links

--- app.js
+++ app.js
@@ -11,13 +11,18 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+var _ = require('lodash');
+// var read=require('readmore-js');
+// app.use(read);
+// var length = 60;
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+// var myTruncatedString = homeStartingContent.substring(0,length);
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 var posts=[];
 
 app.get('/', function(req, res) {
-  // console.log(res.cookie());
+  console.log(posts);
   res.render("home",{
     HomeSartingContent:homeStartingContent,
     posts:posts
@@ -29,17 +34,20 @@ app.get('/about', function(req, res) {
 });
 
 app.get('/post/:newblog', function(req, res){
-  console.log(req.params.newblog);
+  // console.log(req.params.newblog);
   posts.forEach(function(p){
-    if(p.post==req.params.newblog)
+    if( _.lowerCase(p.post)===_.lowerCase(req.params.newblog))
     {
       console.log("Found");
-    }else {
-      console.log("Not Found");
+      res.render("post",{Name:p.post,Content:p.Blog})
     }
+
+    // else {
+    //   let content="Soory ! We Dont Have Any Blog Of "+p.post;
+    //   console.log("Not Found");
+    //   res.render("post",{Name:p.post,Content:content}) 
+    // }
   })
-
-
 })
 
 app.get('/contact', function(req, res) {
@@ -56,7 +64,7 @@ app.post('/compose',function(req, res) {
   }
   posts.push(postObj);
   res.redirect('/');
-  // console.log(postObj);/
+  // console.log(postObj);
 });
 
 
