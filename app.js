f4677da947659625aee49c5613e8a75ee00c20e0
bhavyapatel <bhavyapatel4012@gmail.com>
2023-01-08 16:12:32 +0530

    Save The Blog Into Database Now Solving the id automation

--- app.js
+++ app.js
@@ -3,31 +3,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
 const app = express();
+// Database
+const mongoose = require('mongoose');
+mongoose.set('strictQuery',false);
+mongoose.connect("mongodb://127.0.0.1:27017/BlogDB",{useNewUrlParser: true});
+//Making Schema
+const blog_schema=new mongoose.Schema({
+  name:String,
+  blog:String
+})
+//post collection
+const Post=mongoose.model("blog",blog_schema);
+
+const id_schema=new mongoose.Schema({
+  count:Number
+})
+const id=mongoose.model("id",id_schema);
+// const new_id=new id({
+// count:1
+// });
+// new_id.save()
+
+
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
 var _ = require('lodash');
-// var read=require('readmore-js');
-// app.use(read);
-// var length = 60;
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-// var myTruncatedString = homeStartingContent.substring(0,length);
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 var posts=[];
 
+
+//Routes
 app.get('/', function(req, res) {
   console.log(posts);
   res.render("home",{
     HomeSartingContent:homeStartingContent,
     posts:posts
   });
-  
+
 });
 app.get('/about', function(req, res) {
   res.render("about",{AboutPage:aboutContent});
@@ -48,7 +65,6 @@ app.get('/post/:newblog', function(req, res){
       console.log("Found");
       res.render("post",{Name:p.post,Content:p.Blog})
     }
-
     // else {
     //   let content="Soory ! We Dont Have Any Blog Of "+p.post;
     //   console.log("Not Found");
@@ -61,15 +77,22 @@ app.get('/contact', function(req, res) {
   res.render("contact",{ContactPage:contactContent});
 });
 app.get('/compose', function(req, res) {
+ 
   res.render("compose");
 });
 
 app.post('/compose',function(req, res) {
-  const postObj={
-    post:req.body.blog,
-    Blog:req.body.fullblog,
-  }
-  posts.push(postObj);
+  //Making Document And Save Into Database 
+  //Take The Value From id data base stor into id variable and add 1 and use for new post variable---Todo
+  const new_post=new Post({
+    name:req.body.blog,
+    blog:req.body.fullblog
+  });
+
+  //update the id value into database
+
+  new_post.save()
+  //Store The Blog Into Database
   res.redirect('/');
   // console.log(postObj);
 });
